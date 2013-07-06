7483ba23dfa4349a2225b8640ef837401d74a8a9
steve donovan <steve.j.donovan@gmail.com>
2013-07-06 11:27:27 +0200

    go fmt does not like go.doc

--- doc.go
+++ doc.go
@@ -1,51 +1,52 @@
-/*  luar provides a more convenient way to access Lua from Go, using
+/*  
+    luar provides a more convenient way to access Lua from Go, using
     Alessandro Arzilli's  golua (https://github.com/aarzilli/golua).
     Plain Go functions can be registered with luar and they will be called by reflection;
     methods on Go structs likewise.
 
-    package main
+     package main
 
-   import "fmt"
-   import "github.com/stevedonovan/luar"
+    import "fmt"
+    import "github.com/stevedonovan/luar"
 
-   type MyStruct struct {
-     Name string
-     Age int
-   }
-
-
-   const test = `
-   for i = 1,5 do
-       Print(MSG,i)
-   end
-   Print(ST)
-   print(ST.Name,ST.Age)
-   --// slices!
-   for i,v in pairs(S) do
-      print(i,v)
-   end
-   `
-
-   func main() {
-       L := luar.Init()
-       defer L.Close()
-
-       S := []string {"alfred","alice","bob","frodo"}
-
-       ST := &MyStruct{"Dolly",46}
-
-       luar.Register(L,"",luar.Map{
-           "Print":fmt.Println,
-           "MSG":"hello",  // can also register constants
-           "ST":ST, // and other values
-           "S":S,
-       })
-
-       L.DoString(test)
-
+    type MyStruct struct {
+      Name string
+      Age int
     }
 
-   Go types like slices, maps and structs are passed over as Lua proxy objects,
-   or alternatively copied as tables.
+
+    const test = `
+    for i = 1,5 do
+        Print(MSG,i)
+    end
+    Print(ST)
+    print(ST.Name,ST.Age)
+    --// slices!
+    for i,v in pairs(S) do
+       print(i,v)
+    end
+    `
+
+    func main() {
+        L := luar.Init()
+        defer L.Close()
+
+        S := []string {"alfred","alice","bob","frodo"}
+
+        ST := &MyStruct{"Dolly",46}
+
+        luar.Register(L,"",luar.Map{
+            "Print":fmt.Println,
+            "MSG":"hello",  // can also register constants
+            "ST":ST, // and other values
+            "S":S,
+        })
+
+        L.DoString(test)
+
+     }
+
+    Go types like slices, maps and structs are passed over as Lua proxy objects,
+    or alternatively copied as tables.
 */
 package luar
